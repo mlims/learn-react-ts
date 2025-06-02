0155af33e01fab294367d0c512c5e9c90fa7e571
Matheus Lima <omatheusdasilvalima@gmail.com>
2025-06-02 16:54:29 -0300

    add the card and update with properties in components

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,28 +1,17 @@
 import { Project } from './Project'; 
+import ProjectCard from './ProjectCard';
 
 interface ProjectListProps {
     projects: Project[];
 }
 
 function ProjectList({ projects }: ProjectListProps) {
-    return (
-        <div className="row">
-            {projects.map((Project) => (
-                <div key={Project.id} className="cols-sm">
-                    <div className="card">
-                        <img src={Project.imageUrl} alt={Project.name} />
-                        <section className="section dark">
-                            <h5 className="strong">
-                                <strong>{Project.name}</strong>
-                            </h5>
-                        <p>{Project.description}</p>
-                        <p>Budget: {Project.budget.toLocaleString()}</p>
-                        </section>
-                    </div>
-                </div>
-            ))}
+    const items = projects.map(Project => (
+        <div key={Project.id} className="cols-sm">
+            <ProjectCard project={Project} />
         </div>
-    );
+    ));
+    return <div className="row">{items}</div>
 }
 
 export default ProjectList;
